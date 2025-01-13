6ba284ab36ad58620cf0d9059897fc51f7fbd1ac
Amir <urazaev90@mail.ru>
2025-01-13 17:52:16 +0300

    Отключение вызова контекстного меню и выделений в панелях (2)

---
+++ static/js/disabling-context-menu.js
@@ -0,0 +1,17 @@
+// Отключение контекстного меню на странице
+document.addEventListener('contextmenu', (event) => {
+    event.preventDefault();
+});
+
+// Отключение длительных нажатий на мобильных устройствах
+document.addEventListener('touchstart', (event) => {
+    if (event.touches.length > 1) { // Если больше одного пальца, предотвращаем
+        event.preventDefault();
+    }
+});
+
+document.addEventListener('touchend', (event) => {
+    if (event.touches.length === 0) {
+        event.preventDefault();
+    }
+});
