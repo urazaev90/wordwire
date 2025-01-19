e5866823a887b5fc10f782f383630ebd3fcbb8fc
Amir <urazaev90@mail.ru>
2025-01-19 12:34:04 +0300

    рефакторинг серверного кода (6)

--- main.go
+++ main.go
@@ -28,28 +28,28 @@ func main() {
 
 	router := mux.NewRouter()
 
+	router.NotFoundHandler = http.HandlerFunc(core.CustomNotFoundHandler)
+
 	router.PathPrefix("/static/css/").Handler(http.StripPrefix("/static/css/", http.FileServer(http.Dir("static/css/"))))
 	router.PathPrefix("/static/js/").Handler(http.StripPrefix("/static/js/", http.FileServer(http.Dir("static/js/"))))
 	router.PathPrefix("/static/images/").Handler(http.StripPrefix("/static/images/", http.FileServer(http.Dir("static/images/"))))
 	router.PathPrefix("/static/sounds/").Handler(http.StripPrefix("/static/sounds/", http.FileServer(http.Dir("static/sounds/"))))
 
-	router.NotFoundHandler = http.HandlerFunc(core.CustomNotFoundHandler)
-
 	router.Handle("/captcha/{captchaID}.png", captcha.Server(captcha.StdWidth, captcha.StdHeight))
 
+	router.HandleFunc("/generate-captcha", core.GenerateCaptchaHandler).Methods("GET")
+	router.HandleFunc("/api/get_user_login", core.GetUserLoginHandler).Methods("GET")
 	router.HandleFunc("/", core.RegisterHandler).Methods("GET", "POST")
+	router.HandleFunc("/check-login", core.CheckLoginHandler).Methods("GET", "POST")
 	router.HandleFunc("/login", core.LoginHandler).Methods("GET", "POST")
 	router.HandleFunc("/logout", core.LogoutHandler).Methods("POST")
+	router.HandleFunc("/teaching", core.TeachingPageHandler).Methods("GET")
+	router.HandleFunc("/api/words", core.WordsAPIHandler).Methods("GET")
 	router.HandleFunc("/dictionary", core.DictionaryHandler).Methods("GET", "POST")
 	router.HandleFunc("/selected", core.SelectedHandler).Methods("GET", "POST")
 	router.HandleFunc("/archive", core.ArchiveHandler).Methods("GET", "POST")
 	router.HandleFunc("/remove_from_archive", core.RemoveFromArchiveHandler).Methods("GET", "POST")
 	router.HandleFunc("/add_to_archive", core.AddToArchiveHandler).Methods("GET", "POST")
-	router.HandleFunc("/teaching", core.TeachingPageHandler).Methods("GET")
-	router.HandleFunc("/api/words", core.WordsAPIHandler).Methods("GET")
-	router.HandleFunc("/api/get_user_login", core.GetUserLoginHandler).Methods("GET")
-	router.HandleFunc("/check-login", core.CheckLoginHandler).Methods("GET", "POST")
-	router.HandleFunc("/generate-captcha", core.GenerateCaptchaHandler).Methods("GET")
 
 	router.HandleFunc("/robots.txt", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "robots.txt")
